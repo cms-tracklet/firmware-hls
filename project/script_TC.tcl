ced238d8d8ab4c88fafb52d05753f958978b17f1
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-07-22 12:32:25 -0600

    Minimum changes necessary to run vitis_hls

--- project/script_TC.tcl
+++ project/script_TC.tcl
@@ -3,6 +3,9 @@
 #   vivado_hls -p trackletCalculator
 # WARNING: this will wipe out the original project by the same name
 
+# get some information about the executable and environment
+source env_hls.tcl
+
 # create new project (deleting any existing one of same name)
 open_project -reset trackletCalculator
 
@@ -12,7 +15,11 @@ set_top TrackletCalculator_L1L2G
 add_files ../TrackletAlgorithm/TrackletCalculator.cc -cflags "$CFLAGS"
 add_files -tb ../TestBenches/TrackletCalculator_L1L2G_test.cpp -cflags "$CFLAGS"
 
-open_solution "solution1"
+if { ([string first "vitis" $exe] > -1) && ($year > 2019) } {
+    open_solution "solution1" -flow_target vivado
+} else {
+    open_solution "solution1"
+}
 
 # Define FPGA, clock frequency & common HLS settings.
 source settings_hls.tcl
@@ -21,7 +28,14 @@ source settings_hls.tcl
 add_files -tb ../emData/TC/tables/
 add_files -tb ../emData/TC/TC_L1L2G/
 
-csim_design -compiler gcc -mflags "-j8"
+switch -glob -- $exe {
+    *vitis* {
+        csim_design -mflags "-j8"
+    }
+    default {
+        csim_design -compiler gcc -mflags "-j8"
+    }
+}
 csynth_design
 cosim_design 
 export_design -format ip_catalog
